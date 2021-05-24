b8bcc8f2771e32bfccb6d6e54c44f9a30b998067
Douglas Gaskell <1400380+douglasg14b@users.noreply.github.com>
2021-05-23 23:11:05 -0700

    Adds DevDbSeeder.
    Issues with previous seeding:
    
    1. You cannot define the Ids of transactions when the DB generates those Ids
    2. You cannot define the Vehicle Id in the transaction when that id doesn't exist yet
    3. No need to add the transactions to the DB one-by-one. If they are added to the vehicle, the entire object graph is inserted into the DB when it's added

---
+++ FuelTrackerApi/Data/DevDbSeeder.cs
@@ -0,0 +1,81 @@
+﻿using FuelTrackerApi.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FuelTrackerApi.Data
+{
+    public class DevDbSeeder
+    {
+        private readonly AppDbContext _context;
+
+        public DevDbSeeder(AppDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task SeedDatabase()
+        {
+
+        }
+
+        private async Task SeedVehicles()
+        {
+            DateTime date = DateTime.Parse("2021-05-10");
+
+            Vehicle newVehicle = new Vehicle
+            {
+                Nickname = "Corolla",
+                Make = "Toyota",
+                Model = "Corolla",
+                Year = 2017,
+                Transactions = new List<FuelTransaction>()
+            };
+
+            newVehicle.AddTransaction(new FuelTransaction
+            {
+                Vehicle = newVehicle,
+                Store = "Murphy USA",
+                Date = date,
+                Octane = 87,
+                Range = 119,
+                Distance = 182.7m,
+                Gallons = 7.17m,
+                Odometer = 23415.7m,
+                Price = 2.179m
+            });
+
+            newVehicle.AddTransaction(new FuelTransaction
+            {
+                Vehicle = newVehicle,
+                Store = "Murphy USA",
+                Date = date,
+                Octane = 87,
+                Range = 519,
+                Distance = 500.7m,
+                Gallons = 50.17m,
+                Odometer = 500.7m,
+                Price = 50.179m
+            });
+
+            newVehicle.AddTransaction(new FuelTransaction
+            {
+                Vehicle = newVehicle,
+                Store = "Murphy USA",
+                Date = date,
+                Octane = 87,
+                Range = 219,
+                Distance = 200.7m,
+                Gallons = 20.17m,
+                Odometer = 200.7m,
+                Price = 20.179m
+            });
+
+            _context.Vehicles.Add(newVehicle);
+            _context.SaveChangesAsync();
+        }
+
+
+    }
+}
