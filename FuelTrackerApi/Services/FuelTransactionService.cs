0d930f4c505af5a700a13dc9ecd48bda26dd2a00
Douglas Gaskell <1400380+douglasg14b@users.noreply.github.com>
2021-05-23 23:22:34 -0700

    Adds Services to replace pseudo-repositories
    There is no need to have the pseudo-repositories. The repository pattern already exists with entity framework. Any guide that says differently was written by someone still too green with the architecture to know better.
    
    It also violates SOC, and create architectural issues down the road.

---
+++ FuelTrackerApi/Services/FuelTransactionService.cs
@@ -0,0 +1,58 @@
+﻿using FuelTrackerApi.Data;
+using FuelTrackerApi.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FuelTrackerApi.Services
+{
+    public class FuelTransactionService
+    {
+        private readonly AppDbContext _context;
+
+        public FuelTransactionService(AppDbContext fuelTransactionContext)
+        {
+            _context = fuelTransactionContext;
+        }
+
+        public void CreateFuelTransaction(FuelTransaction fuelTransaction)
+        {
+            if (fuelTransaction is null)
+                throw new ArgumentNullException(nameof(fuelTransaction));
+
+            _context.FuelTransactions.Add(fuelTransaction);
+        }
+
+        public void DeleteFuelTransaction(FuelTransaction fuelTransaction)
+        {
+            if (fuelTransaction == null)
+                throw new ArgumentNullException(nameof(fuelTransaction));
+
+            _context.FuelTransactions.Remove(fuelTransaction);
+        }
+
+        public IEnumerable<FuelTransaction> GetAllFuelTransactions()
+        {
+            return _context
+                .FuelTransactions
+                .Include(x => x.Vehicle)
+                .ToList();
+        }
+
+        public FuelTransaction GetFuelTransactionById(int id)
+        {
+            return _context
+                .FuelTransactions
+                .Include(x => x.Vehicle)
+                .Where(x => x.Id == id)
+                .FirstOrDefault();
+        }
+
+        public void UpdateFuelTransaction(int id, FuelTransaction fuelTransaction)
+        {
+            //Nothing
+        }
+    }
+}
---
+++ FuelTrackerApi/Services/VehicleService.cs
@@ -0,0 +1,53 @@
+﻿using FuelTrackerApi.Data;
+using FuelTrackerApi.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FuelTrackerApi.Services
+{
+    public class VehicleService
+    {
+        private readonly AppDbContext _context;
+
+        public VehicleService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateVehicle(Vehicle vehicle)
+        {
+            if (vehicle == null)
+                throw new ArgumentNullException(nameof(vehicle));
+
+            _context.Vehicles.Add(vehicle);
+            _context.SaveChanges();
+        }
+
+        public void DeleteVehicle(Vehicle vehicle)
+        {
+            if (vehicle == null)
+                throw new ArgumentNullException(nameof(vehicle));
+
+            _context.Vehicles.Remove(vehicle);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Vehicle> GetAllVehicles()
+        {
+            return _context.Vehicles.ToList();
+        }
+
+        public Vehicle GetVehicleById(int id)
+        {
+            return _context.Vehicles.FirstOrDefault(v => v.Id == id);
+        }
+
+        public void UpdateVehicle(int id, Vehicle vehicle)
+        {
+            //Nothing 
+        }
+
+    }
+}
